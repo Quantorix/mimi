1f17408beb344a411340c2e5defc9423c48abf5d
daverbj <dave@quantorix.com>
2021-03-13 02:45:20 +0530

    adding reseizable in draggble

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './App.css'
 import Draggable from 'react-draggable';
 import {PathLine} from 'react-svg-pathline'
+import { Resizable, ResizableBox } from 'react-resizable';
+import 'react-resizable/css/styles.css'
 class App extends React.Component {
   constructor(){
     super()
@@ -15,8 +17,8 @@ class App extends React.Component {
         y:0
       },
       end:{
-        x:350,
-        y:100
+        x:0,
+        y:0
       }
     }
   }
@@ -36,8 +38,6 @@ class App extends React.Component {
   }, false);
   }
   render(){
-    console.log('start', this.state.start.x, this.state.start.y)
-    console.log('end', this.state.end.x, this.state.end.y)
     return(
       <div className="container">
         <Draggable bounds="parent" disabled={this.state.disabled} onDrag={(e, data) => {
@@ -48,16 +48,27 @@ class App extends React.Component {
             <div className="handle" 
               onMouseDown={(e) => {
                 e.stopPropagation()
-                this.setState({disabled: true, isConnecting: true, start:{x:e.clientX, y:e.clientY}})
+                this.setState({disabled: true, isConnecting: true, start:{x:e.clientX, y:e.clientY}, end:{x:e.clientX, y:e.clientY}})
               }}
             ></div>
           </div>
           </Draggable>
-        <Draggable onDrag={(e, data) => {
-          this.setState({f2: {x: data.x, y:data.y}})
-        }}><div id="f2">
-            x: {this.state.f2.x.toFixed(0)}, y: {this.state.f2.y.toFixed(0)}
-          </div></Draggable>
+          <Draggable disabled={this.state.disabled}>
+            <ResizableBox className="box" width={200} height={120} draggableOpts={{grid: [25, 25]}} 
+              onResize={(e, {element, size}) => {
+                e.stopPropagation()
+                console.log('resize')
+                this.setState({disabled: true})
+              }}
+              onResizeStart={(e) => {
+                e.stopPropagation()
+                console.log('resize start')
+                this.setState({disabled: true})
+              }}
+            >
+              <span className="text">Resizable rectangle with a locked aspect ratio.</span>
+            </ResizableBox>
+          </Draggable>
         <svg className='drawing_container'>
           <PathLine 
               points={[{x:this.state.start.x, y:this.state.start.y}, {x:this.state.end.x, y: this.state.end.y}]} 
